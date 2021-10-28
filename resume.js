d5413721e02cc48c5a64902904bfe2ce19f937e9
Yann Dev <h.yann_pro@yahoo.fr>
2021-10-28 13:48:28 +0200

    Media queries added, remove bold class

--- resume.js
+++ resume.js
@@ -3,10 +3,6 @@ const themeBtnDark = document.getElementById("dark");
 
 var themeColor = "dark"; // Default theme color
 
-window.onload = function() {
-    setThemeColor("dark");
-};
-
 themeBtnLight.addEventListener("click", function (e) {
     var themeColor = "light";
     setThemeColor(themeColor);
@@ -25,15 +21,22 @@ function setThemeColor(themeColor) {
     }
     else if(themeColor) {
         if(themeColor === "light") {
-            html.style.cssText = `--main-color: #1895CE;--main-bg-color: #FFFFFF;
-            --main-text-color: #00000;--secondary-text-color: #FFFFFF;--webkit-track-color: #1D749B`;
+            html.style.cssText = `
+            --main-color: #1895CE;
+            --main-bg-color: #FFFFFF;
+            --main-text-color: #00000;
+            --secondary-text-color: #FFFFFF;
+            --webkit-track-color: #1D749B
+            `;
         }
         else {
-            html.style.cssText = "--main-color: #8F5BFD";
-            html.style.cssText = "--main-bg-color: #0D1117";
-            html.style.cssText = "--main-text-color: #FFFFFF";
-            html.style.cssText = "--secondary-text-color: #0D1117";
-            html.style.cssText = "--webkit-track-color: #5828be";
+            html.style.cssText = `
+            --main-color: #8F5BFD;
+            --main-bg-color: #0D1117;
+            --main-text-color: #FFFFFF;
+            --secondary-text-color: #0D1117;
+            --webkit-track-color: #5828be;
+            `;
         }
     }
 }
