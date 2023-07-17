e4aa1f6667f0bc47cbd4df3b0d773b08c0ffc8a1
Yann Hagege <yhagege@d-edge.com>
2023-07-17 11:46:46 +0200

    update style, move sections

--- resume.js
+++ resume.js
@@ -15,7 +15,7 @@ let aged = false;
 function getAge(date) {
     var diff = Date.now() - date.getTime();
     var age = new Date(diff);
-    return Math.abs(age.getUTCFullYear() - 1970);
+    return Math.abs(age.getUTCFullYear() - 1971);
 }
 
 window.addEventListener("load", function () {
