470009a95a125f43c1d0eb237070eca0219635fe
Yann <79877110+Yann-dv@users.noreply.github.com>
2024-02-19 22:15:49 +0100

    chore: update pdf printer

--- resume.js
+++ resume.js
@@ -87,21 +87,20 @@ print_pdf.addEventListener("click", function () {
     <style> .fullname {font-size:22px} .pitch {font-size: 16px; width: 90%; margin-bottom: 3rem; margin-top: 2rem} 
     h2 {font-size: 14px} .jobdesc {width: 100%} .jobgoal {font-size: 20px} 
     .left-side h3{font-size: 20px; margin-bottom: .5rem} .left-side h4 {font-size: 18px} .left-side p {font-size: 14px} 
-    .left-side .diploma-level {font-size: 10px} .skills_progressbar ul li, .lang ul li, .hobbies ul li { margin-left: -20px} 
+    .left-side .diploma-level {font-size: 10px} .skills_progressbar ul li, .hobbies ul li { margin-left: -20px} 
     .lang h3, .hobbies h3 {margin-bottom: .2rem}
-    .lang > ul:nth-child(1) > li:nth-child(3) > input:nth-child(1) {display: none;}
-    #lang > ul:nth-child(1) > li:nth-child(4) {display: none;}
-    .lang > ul:nth-child(1) > li:nth-child(5) {display: none;}
+    .lang h3 {margin-left: -20px; margin-top: .5rem}
     .personal-infos > ul li {margin-top: 2px; margin-bottom: 1px} 
     #hobbies {display: none}
     .skills, formations-list, experiences-list {font-size: 12px} .rigth-side {font-size: 10px}
-    [type='range'] {margin: 0; height: .7rem} .right-side-subtitle {margin-bottom: 0.5rem, margin-top: 0.2rem} h3 {font-size: 20px; margin-top: -30px} 
+    [type='range'] {margin: 0; height: .7rem} .right-side-subtitle {margin-bottom: 0.5rem, margin-top: 0.2rem} h3 {font-size: 16px; margin-top: -10px} 
     .right-side h4 {font-size: 16px; margin: .1rem} .socials i.fab:nth-child(n+1), .socials i.fa-solid:nth-child(1n+1) {font-size: 3rem} 
     .right-side-header li {font-size: 16px} .right-side-header li i {font-size: 20px}
     .personal-infos > ul li i {font-size: 20px} .personal-infos > ul li {font-size: 16px; margin-left: -20px} 
-    .formations-list-item, .experiences-list-item {margin-top: -15px} .skills_progressbar li {margin-bottom: 0} .skills_progressbar li, .lang li, 
+    .formations-list-item, .experiences-list-item {margin-top: -15px} .skills_progressbar li {margin-bottom: 0} .skills_progressbar li, 
     .hobbies li {margin-top : -5px} .right-side {height : 98%} .level-bar {height: 1.8vh; border-radius: 4px; font-size: 14px;} .formations h3 {margin : 10px}
-    .skills_progressbar img, .lang img, .hobbies img{width: 20px; height: 20px; margin-right: 5px}
+    .skills_progressbar img, .languages img, .hobbies img{width: 20px; height: 20px; margin-right: 5px}
+    .languages {font-size: 14px; margin: 1.5rem}
     .experiences {margin-top: 40px}
     .theme-button {display: none}
     `;
